6fedfbed998fb460cb8249de4f22aaea1dddd2f7
Shane Exterkamp <shaneexterkamp5@gmail.com>
2018-04-11 22:24:31 -0400

    [Feature] CSS and Material UI

--- fullstack_template/static/js/index.jsx
+++ fullstack_template/static/js/index.jsx
@@ -1,5 +1,9 @@
 // index.jsx
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
-ReactDOM.render(<App />, document.getElementById("content"));
+//import App from "./App";
+import SimpleAppBar from "./SimpleAppBar";
+
+require('../css/main.css');
+
+ReactDOM.render(<SimpleAppBar />, document.getElementById("content"));
